b99ab0a028d43dba9fd8802da836500b1fc9c75c
AdminOffice <nattavutmail3@gmail.com>
2022-10-12 10:56:26 +0700

    เริ่มต้นการใช้งาน props

--- chap2/MyApplication_2/my-app/src/components/ItemComponent.js
+++ chap2/MyApplication_2/my-app/src/components/ItemComponent.js
@@ -1,4 +1,9 @@
 const ItemComponent =(props)=>{
+  /**
+   * Props หรือชื่อเต็มๆมันคือ Properties หากเปรียบกับ HTML แล้ว 
+   * ตัว Props จะเป็นคล้ายๆ attributes ของ HTML ดัง เช่น src, href หรือ class
+   * 
+   */
   return(
       <>
          <li >{props.name} <span>{props.amount}</span></li>
